91ddf3b2caf9733e36774c9a6323479b69f2ea66
Vasil-Sh <vasilshchigil@gmail.com>
2026-07-21 14:19:15 +0300

    chore: update backend submodule and add commit script

---
+++ commit-cs2.ps1
@@ -0,0 +1,8 @@
+Set-Location d:\github\mathciq\backend
+git add -A
+git commit -m "feat: CS2 scraper on tips.gg as backup source"
+git push
+Set-Location d:\github\mathciq
+git add backend
+git commit -m "chore: updated backend (CS2 scraper)"
+git push
