005ca811c1ac0bc1467dad4f7dea13f8952a981b
Vasil-Sh <vasilshchigil@gmail.com>
2026-07-22 16:59:51 +0300

    fix: add missing DialogFooter import in RiskManagement

--- src/components/RiskManagement.tsx
+++ src/components/RiskManagement.tsx
@@ -33,6 +33,7 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
+  DialogFooter,
 } from "@/components/ui/dialog";
 import { googleSheetsRiskyTeamsService } from "@/lib/googleSheetsRiskyTeams";
 import { logRender } from "@/lib/devLogger";
