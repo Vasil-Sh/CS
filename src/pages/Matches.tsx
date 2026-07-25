20d9ac249839fb876866c2cad2d3a369f08f7907
Vasil-Sh <vasilshchigil@gmail.com>
2026-07-25 11:56:08 +0300

    fix: replace space-y-6 with gap-y-6 in Matches page (space-y doesnt work in flex container)

--- src/pages/Matches.tsx
+++ src/pages/Matches.tsx
@@ -183,7 +183,7 @@ export default function Matches() {
           onToggleTheme={toggleTheme}
           showThemeToggle={false}
         />
-        <div className="relative z-10 space-y-6 px-6 lg:px-8 pb-8 pt-4 flex flex-col flex-1 min-h-0">
+        <div className="relative z-10 flex flex-col gap-y-6 px-6 lg:px-8 pb-8 pt-4 flex-1 min-h-0">
           <DateStatsCards
             displayCount={m.displayCount}
             cs2DisplayedCount={m.cs2DisplayedCount}
